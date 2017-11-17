a09c100498aeb481bd4409d49c4d60e63a6259a8
林永璋 <yclin@jumbogames.com.tw>
2017-11-17 18:35:53 +0800

    add deviceID shell

---
+++ deviceIDs.sh
@@ -0,0 +1,48 @@
+xcrunReturn=$(xcrun simctl list)
+
+declare -a saveText=()
+declare -a excludeText=('==' '(Shutdown)' '(unavailable)')
+
+status=0
+
+for e in $xcrunReturn
+do
+    if [ "$status" == 1 ] ; then
+
+        if [ "$e" == 'tvOS' ]; then
+            break
+        fi
+        
+        isSave=true
+        for exclude in "${excludeText[@]}"
+        do
+            if [ "$e" == "$exclude" ]; then
+                isSave=false
+                break
+            fi
+        done
+
+        if [ "$isSave" == true ]; then
+            saveText+=($e)
+        fi
+        
+    else
+        if [ "$e" == 'Devices' ]; then
+            status=1
+        fi
+    fi
+done
+
+declare -a array_ID=()
+
+for e in "${saveText[@]}"
+do
+    if [ ${#e} == 38 ]; then
+        array_ID+=(${e:1:36})
+    fi
+done
+
+for e in "${array_ID[@]}"
+do
+    echo $e
+done
